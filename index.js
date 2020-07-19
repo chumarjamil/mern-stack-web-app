8e627d90f845b65de609386c3f0ef7555ba3ef39
Umar Jamil <umar.jamil@img-advisors.com>
2020-07-19 20:17:09 +0800

    Version 1.1

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ bye: 'Umar' });
-});
+
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecert,
+    callbackURL: '/auth/google/callback'
+}, (accessToken) => {
+    console.log(accessToken);
+}));
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
