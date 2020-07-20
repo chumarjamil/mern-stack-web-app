d125773f3df088a80d9335a7eb895530d2e54904
Umar Jamil <umar.jamil@img-advisors.com>
2020-07-20 09:39:16 +0800

    Refactor & Routes configuration

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    googleClientID: 'Your Client ID',
-    googleClientSecert: 'Your Client Secert'
+    googleClientID: '238251468741-5lofds85o2d8hkgnn872244h15a443cl.apps.googleusercontent.com',
+    googleClientSecert: 'wTxh9Aq8mYjs-j8BuvImCBAG'
 };
