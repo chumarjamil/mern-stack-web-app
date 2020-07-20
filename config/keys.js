0f4ff18a957381894a9f439113dad0b07c616cf6
Umar Jamil <umar.jamil@img-advisors.com>
2020-07-20 16:11:17 +0800

    V 1.0

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,7 @@
 module.exports = {
     googleClientID: 'YOUR CLIENT ID',
-    googleClientSecert: 'YOUR CLIENT SECERT'
+    googleClientSecert: 'YOUR CLIENT SECERT',
+
+    //MongoDB URI
+    mongoURI: 'YOUR mongoDB URL for Connectivity'
 };
--- services/passport.js
+++ services/passport.js
@@ -1,13 +1,32 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('user');
+
+// passport.serializeUser((user, done) =>{
+
+// });
 
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecert,
     callbackURL: '/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
-    console.log('access token', accessToken);
-    console.log('refresh token', refreshToken);
-    console.log('profile', profile);
+    // console.log('access token', accessToken);
+    // console.log('refresh token', refreshToken);
+    // console.log('profile', profile);
+    User.findOne({ googleId: profile.id })
+        .then((existingUser) => {
+            if (existingUser) {
+                //means we have record alread
+                done(null, existingUser);
+            } else {
+                new User({ googleId: profile.id })
+                    .save()
+                    .then(user => done(null, user));
+            }
+        })
+
 }));
